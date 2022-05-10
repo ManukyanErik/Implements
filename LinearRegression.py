11a9be1b05e290339f18d9e3c27e9cd50c6b47ba
Erik <erik.55121616@gmail.com>
2022-05-11 01:52:55 +0400

    add Linear Regression

---
+++ LinearRegression.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self, lr=0.01,  max_iter=500, metric_change=0.001):
+        self.lr = lr
+        self.max_iter = max_iter
+        self.metric_change = metric_change
+    
+    def gradient(self, x, w, y):
+        derivative = []
+        for i in range(w.shape[0]):
+            derivative_i = 2 * np.mean(((x @ w.T) - y) * x[:, i])
+            derivative.append(derivative_i)
+        return np.array(derivative)
+
+    def update_weights(self, x, w, y):
+        derivatives = self.gradient(x, w, y)
+        w = w - self.lr * derivatives
+        return w
+
+    def fit(self, X_train, y_train):
+        self.X_train = np.array(X_train)
+        self.y_train = np.array(y_train)
+
+        ones = np.ones(self.X_train.shape[0]).reshape(-1, 1)
+        x = np.hstack((ones, self.X_train)) 
+        w = np.random.random(x.shape[1])
+        
+        loss = np.mean(((x @ w.T) - self.y_train)**2)
+
+        for _ in range(self.max_iter):
+            self.update_weights(x, w, self.y_train)
+            new_loss = np.mean(((x @ w.T) - self.y_train)**2)
+            temp_change = loss - new_loss
+            # if temp_change < 0:
+            #     raise ValueError('Bad learning rate or lambda, loss function increases')
+            loss = new_loss
+            if temp_change < self.metric_change:
+                break
+        
+        self.W = w
+
+    def predict(self, X_test):
+        ones = np.ones(X_test.shape[0]).reshape(-1, 1)
+        X_test = np.hstack((ones, X_test))
+        y_pred = X_test @ self.W.T
+        
+        return y_pred
+
+        
